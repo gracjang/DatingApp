f463bd6425567d738c736d46f0f933a4174714b8
Gracjan <graku12@gmail.com>
2019-04-01 08:38:39 +0200

    Full login and register event

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -18,6 +18,10 @@ login(model: any) {
         localStorage.setItem('token', user.token);
       }
     })
-  )
+  );
 }
+
+  register(model: any){
+    return this.http.post(this.baseUrl + 'register', model);
+  }
 }
--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,12 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-registerMode = false;
-  constructor() { }
-
+  registerMode = false;
+  constructor(private http: HttpClient) { }
   ngOnInit() {
+
   }
   registerToggle() {
-    this.registerMode = !this.registerMode;
+    this.registerMode = true;
+  }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
   }
 }
